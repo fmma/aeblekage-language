770fb3acfab517c51be0f71cdc4f4f568ef0e511
Frederik Madsen <frederik.meisner@gmail.com>
2020-12-14 14:20:02 +0100

    Added member type long syntax.

--- src/parser/astParser.ts
+++ src/parser/astParser.ts
@@ -4,6 +4,8 @@ import { Import } from "../types/ast/import";
 import { Interface } from "../types/ast/interface";
 import { Member } from "../types/ast/member";
 import { MemberType } from "../types/ast/memberType";
+import { Type } from "../types/ast/type";
+import { Tfun } from "../types/ast/type/fun";
 import { parseIdent, parseNewline } from "./common";
 import { exprParser } from "./exprParser";
 import { Parser } from "./parser-combinators";
@@ -15,7 +17,7 @@ export const astImportParser: Parser<Import>
 export const astMemberTypeParser: Parser<MemberType>
     = parseIdent.bind(f =>
         parseIdent.many().bind(as =>
-            Parser.sat(/^: */).pre(typeParser.map(t =>
+            Parser.sat(/^: */).pre(typeParser.choice(astTypeSequenceParser).map(t =>
                 new MemberType(f, as, t)))));
 
 export const astMemberParser: Parser<Member>
@@ -28,6 +30,9 @@ export const astMemberParser: Parser<Member>
 
 export const astExprSequenceParser: Parser<ExprSequence>
     = iseq(exprParser).map(es => new ExprSequence(es));
+
+export const astTypeSequenceParser: Parser<Type>
+    = iseq(typeParser).map(ts => ts.reduceRight((t2, t1) => new Tfun(t1, t2)));
 
 export const astInterfaceParser: Parser<Interface>
     = Parser.do(
@@ -55,5 +60,11 @@ export const astParser = Parser.sat(/\s*/).pre(astInterfaceParser.disjointChoice
 function iseq<A>(p: Parser<A>): Parser<A[]> {
     return Parser.sat(/^\ni */)
         .pre(p.sepby(parseNewline))
+        .post(Parser.sat(/^\nd */)).choice(Parser.pure([]));
+}
+
+function iseq1<A>(p: Parser<A>): Parser<A[]> {
+    return Parser.sat(/^\ni */)
+        .pre(p.sepby1(parseNewline))
         .post(Parser.sat(/^\nd */)).choice(Parser.pure([]));
 }
